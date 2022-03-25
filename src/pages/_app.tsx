d4f7882a2afd8dfd6162415d1da018caf26ae7b5
yungblud <killi8n@gmail.com>
2022-03-25 21:42:03 +0900

    chore: reset css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import emotionReset from 'emotion-reset';
+import { Global, css } from '@emotion/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Global styles={css`
+        ${emotionReset}
+      `} />
+    </>
+  )
 }
 
 export default MyApp
