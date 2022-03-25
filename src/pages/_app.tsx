9082d2bcf1dfd8678b6c9d3d3407271da9738097
yungblud <killi8n@gmail.com>
2022-03-25 21:44:50 +0900

    feat: added prettier, lint fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app'
-import emotionReset from 'emotion-reset';
-import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset'
+import { Global, css } from '@emotion/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Global styles={css`
-        ${emotionReset}
-      `} />
-    </>
-  )
+    return (
+        <>
+            <Component {...pageProps} />
+            <Global
+                styles={css`
+                    ${emotionReset}
+                `}
+            />
+        </>
+    )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <h1>ColdSurf.io / Coming Soon</h1>
-  )
+    return <h1>ColdSurf.io / Coming Soon</h1>
 }
 
 export default Home
