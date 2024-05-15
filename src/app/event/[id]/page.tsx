30b351b4ed714fe8e46d1b724a95873c42836654
yungblud <killi8n@gmail.com>
2024-05-15 21:07:24 +0900

    feat(ui): implemented layout css

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,3 +1,29 @@
+import Image from 'next/image'
+
 export default function EventDetailPage() {
-  return null
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="mt-0 mb-0 ml-auto mr-auto w-6/12">
+        <div className="flex align-top w-full">
+          <div className="sticky top-16" style={{ flex: '1 1 0%' }}>
+            <div className="w-full">
+              <Image
+                src={'https://source.unsplash.com/random/300×300'}
+                alt={'alt!'}
+                width={256}
+                height={256}
+                className="w-full h-auto rounded shadow-md"
+                style={{
+                  aspectRatio: '1 / 1',
+                }}
+              />
+            </div>
+          </div>
+          <div className="ml-12" style={{ flex: '2 1 0%' }}>
+            <h1 className="text-5xl">Title</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
