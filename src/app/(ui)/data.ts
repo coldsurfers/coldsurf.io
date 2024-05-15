f5ea6fa9ec5e151b5b973f8ad494c09fd287cf8b
yungblud <killi8n@gmail.com>
2024-05-15 20:16:59 +0900

    chore: implemented more data

--- src/app/(ui)/data.ts
+++ src/app/(ui)/data.ts
@@ -11,5 +11,29 @@ export const posters = [
     date: 'June 5, 2024',
     venue: 'Venue 2',
   },
+  {
+    title: 'Concert Name 1',
+    image: 'https://source.unsplash.com/random/300×300',
+    date: 'May 20, 2024',
+    venue: 'Venue 1',
+  },
+  {
+    title: 'Concert Name 2',
+    image: 'https://source.unsplash.com/random/300×300',
+    date: 'June 5, 2024',
+    venue: 'Venue 2',
+  },
+  {
+    title: 'Concert Name 1',
+    image: 'https://source.unsplash.com/random/300×300',
+    date: 'May 20, 2024',
+    venue: 'Venue 1',
+  },
+  {
+    title: 'Concert Name 2',
+    image: 'https://source.unsplash.com/random/300×300',
+    date: 'June 5, 2024',
+    venue: 'Venue 2',
+  },
   // Add more poster objects as needed
 ] as const
